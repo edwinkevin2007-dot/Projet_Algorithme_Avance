3fd080eaae7b4d06c6b665e917cc14928f02e101
Edwin <edwinkevin2007@gmail.com>
2026-07-18 01:11:44 +0300

    Fichier de travail de Ritso

---
+++ main.py
@@ -0,0 +1,6 @@
+# ----------------
+# Zone pour Ritso
+# ----------------
+# C'est ici que tu vas créer notre interface textuelle propre
+# (un menu interactif) pour la démonstration vidéo (Vokoscreen)
+# Voici le lien direct pour le télécharger : https://linuxecke.volkoh.de/vokoscreen/vokoscreen-download.html
